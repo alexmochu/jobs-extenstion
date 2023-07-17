b0296a8893c77665645b806bc308427599b1b890
alex.mochu <mochualex4@gmail.com>
2023-07-17 19:12:37 +0300

    setup api endpoints

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import client from './client'
+
+export default {
+  home: {
+    landingPage: () => client.get('/api').then((res) => res.data),
+  },
+  user: {
+    login: (credentials) => client.post('/api/login', { credentials }).then((res) => res.data),
+    logout: () => client.post('/api/logout'),
+    signup: (user) => client.post('/api/register', { user }).then((res) => res.data.user),
+    forgotPassword: (user) =>
+      client.post('/forgot-password', { user }).then((res) => res.data.user),
+  },
+  jobs: {
+    jobsUser: (username) => client.get(`/api/jobs/${username}`, { username }).then((res) => res.data),
+    jobSummary: () => client.get('/api/job_summary').then((res) => res.data)
+  }
+}
---
+++ src/api/queries.js
@@ -0,0 +1,49 @@
+import api from './api'
+import setAuthToken from '../utilities/setAuthToken'
+import removeAuthToken from '../utilities/removeAuthToken'
+
+const landingPage = async () => {
+  const response = await api.home.landingPage()
+  return response
+}
+
+const signup = async (user) => {
+  const response = await api.user.signup(user)
+  return response
+}
+
+const login = async (credentials) => {
+  const response = await api.user.login(credentials)
+  const token = response.header_access_token
+  localStorage.headerAccessToken = token
+  return setAuthToken(token)
+}
+
+const logout = async () => {
+  const response = await api.user.logout()
+  const token = localStorage.getItem('headerAccessToken')
+  removeAuthToken(token)
+  localStorage.removeItem(token)
+  return response
+}
+
+const getCurrentUserJobs = async (username) => {
+  const response = await api.jobs.jobsUser(username)
+  return response
+}
+
+const getJobSummary = async () => {
+  const response = await api.jobs.jobSummary()
+  return response
+}
+
+const Queries = {
+  landingPage,
+  signup,
+  login,
+  logout,
+  getCurrentUserJobs,
+  getJobSummary
+}
+
+export default Queries
