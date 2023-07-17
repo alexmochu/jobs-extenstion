8c5e51e6992f2ebd0dd9388010469466c5493488
alex.mochu <mochualex4@gmail.com>
2023-07-17 19:15:39 +0300

    add state

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { useContext, createContext, useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient()
+
+import { RouterProvider } from 'react-router-dom'
+
+import router from './router.jsx'
+
+const initialState = {
+  username: '',
+  isAuthenticated: false,
+  showToast: false,
+  toastMessage: '',
+}
+
+const CurrentUserState = createContext(initialState)
+
+const App = () => {
+  const [user, setUser] = useState(() => {
+    const state = localStorage.getItem('store')
+    if (state === null) {
+      return initialState
+    } else if (!JSON.parse(state).isAuthenticated) {
+      return initialState
+    } else {
+      return JSON.parse(state)
+    }
+  })
+
+  useEffect(() => {
+    const storeState = localStorage.getItem('store')
+    if (storeState) {
+      setUser(JSON.parse(storeState))
+    }
+  }, [])
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem('store', JSON.stringify(user))
+    }
+  }, [user, user])
+
+  return (
+    <CurrentUserState.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </CurrentUserState.Provider>
+  )
+}
+
+export function userState() {
+  return useContext(CurrentUserState)
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
