1c2cb983b92fc3fb469555e5484fa7948245597d
alex.mochu <mochualex4@gmail.com>
2023-08-17 22:31:08 +0300

    update UI

--- src/routes/changePassword.jsx
+++ src/routes/changePassword.jsx
@@ -8,55 +8,78 @@ import jwt_decode from 'jwt-decode'
 export default function ChangePassword() {
   const { user, setUser } = userState()
   const [inputValue, setInputValue] = useState({
-    username: '',
-    password: '',
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: ''
   })
   const [error, setError] = useState({
-    username: '',
-    password: '',
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: ''
   })
+
   const [loading, setLoading] = useState(false)
 
-  const navigate = useNavigate()
   const handleSubmit = async (event) => {
     event.preventDefault()
-    const { username, password } = inputValue
-    if (username.trim() === '') {
-      setError({ ...error, username: 'Username can\'t be blank' })
-    } else if (password.trim() === '') {
-      setError({ ...error, password: 'Password can\'t be blank' })
-    } else {
+    const { currentPassword, newPassword, confirmPassword } = inputValue
+    if (currentPassword.trim() === '') {
+      setError({ ...error, currentPassword: 'Current password can\'t be blank' })
+    } else if (newPassword.trim() === '') {
+      setError({ ...error, newPassword: 'New password can\'t be blank' })
+    }
+    else if (confirmPassword.trim() === '') {
+      setError({ ...error, confirmPassword: 'Confirm password can\'t be blank' })
+    }
+     else if (newPassword.trim() === currentPassword.trim()) {
+      setError({ 
+        currentPassword: 'Passwords should not be same', 
+        newPassword: 'Passwords should not be same',
+       })
+    }
+    else if (currentPassword.trim() === confirmPassword.trim()) {
+      setError({ 
+        confirmPassword: 'Passwords should not be same', 
+        currentPassword: 'Passwords should not be same',
+       })
+    }
+    else if (newPassword.trim() !== confirmPassword.trim()) {
+      setError({ 
+        confirmPassword: 'Passwords should be same', 
+        newPassword: 'Passwords should be same',
+       })
+    }
+    else {
       // Handle form submission here
       setLoading(true)
-      const user = JSON.parse(localStorage.getItem('store'))
-      await Queries.login(inputValue)
-      const token = localStorage.getItem('headerAccessToken')
-      const decoded = jwt_decode(token)
-      setLoading(false)
-      await setUser({
-        ...user,
-        username: decoded.username,
-        isAuthenticated: true,
-        showToast: true,
-        toastMessage: 'You have logged in successfully.',
+      await Queries.changePassword({
+        current_password: currentPassword,
+        new_password: newPassword,
+        confirm_password: confirmPassword
       })
+      setLoading(false)
+      await setUser(prevState => ({
+      ...prevState,
+      showToast: true,
+      toastMessage: 'Your password has been changed successfully.'
+    }))
       // Reset form
-      setInputValue({ username: '', password: '' })
-      setError({ username: '', password: '' })
-      return navigate('/dashboard')
+      setInputValue({ currentPassword: '', newPassword: '' })
+      setError({ currentPassword: '', newPassword: '' })
+      return 'Success'
     }
   }
 
   const handleChange = (e) => {
     setInputValue({ ...inputValue, [e.target.name]: e.target.value })
-    setError({ username: '', password: '' })
+    setError({ currentPassword: '', newPassword: '' })
   }
 
   return (
     <Fragment>
       <div className='relative'>
         <div className='max-w-7xl mx-auto px-6 md:px-12 xl:px-6'>
-          <div className='relative mt-20 ml-auto'>
+          <div className='relative mt-5 ml-auto'>
             <div className='lg:w-2/3 text-center mx-auto'>
               <label className='block'>
                 <h1 className='text-4xl font-bold tracking-tight mb-2 text-gray-900'>Change Password</h1>
@@ -69,52 +92,77 @@ export default function ChangePassword() {
                 <div className='mt-10 flex flex-wrap justify-center gap-y-4 gap-x-6'>
                   <form onSubmit={handleSubmit}>
                     <label className='block mt-2'>
-                      <span className='block text-left text-sm font-medium text-slate-700'>
-                        Old Password
+                      <span className='block dark:text-white text-left text-sm font-medium text-slate-700'>
+                        Current Password
                       </span>
                       <input
-                        placeholder='Password'
+                        placeholder='Current Password'
                         aria-label='Password'
                         type='password'
-                        name='password'
-                        value={inputValue.password}
+                        name='currentPassword'
+                        value={inputValue.currentPassword}
                         onChange={handleChange}
                         className={`mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                             invalid:border-pink-500 invalid:text-pink-600
                             focus:invalid:border-pink-500 focus:invalid:ring-pink-500  ${
-                              error.password && !inputValue.password ? 'border-red-500' : ''
+                              error.currentPassword && !inputValue.currentPassword ? 'border-red-500' : ''
                             }`}
                       />
-                      {error.password && (
+                      {error.currentPassword && (
                         <span className='block text-left text-sm font-medium text-red-700'>
-                          {error.password}
+                          {error.currentPassword}
                         </span>
                       )}
                     </label>
                     <label className='block mt-2'>
-                      <span className='block text-left text-sm font-medium text-slate-700'>
+                      <span className='dark:text-white block text-left text-sm font-medium text-slate-700'>
                         New Password
                       </span>
                       <input
-                        placeholder='Password'
+                        placeholder='New Password'
                         aria-label='Password'
                         type='password'
-                        name='password'
-                        value={inputValue.password}
+                        name='newPassword'
+                        value={inputValue.newPassword}
                         onChange={handleChange}
                         className={`mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                             invalid:border-pink-500 invalid:text-pink-600
                             focus:invalid:border-pink-500 focus:invalid:ring-pink-500  ${
-                              error.password && !inputValue.password ? 'border-red-500' : ''
+                              error.newPassword && !inputValue.newPassword ? 'border-red-500' : ''
                             }`}
                       />
-                      {error.password && (
+                      {error.newPassword && (
                         <span className='block text-left text-sm font-medium text-red-700'>
-                          {error.password}
+                          {error.newPassword}
+                        </span>
+                      )}
+                    </label>
+                    <label className='block mt-2'>
+                      <span className='dark:text-white block text-left text-sm font-medium text-slate-700'>
+                        Confirm Password
+                      </span>
+                      <input
+                        placeholder='Confirm Password'
+                        aria-label='Password'
+                        type='password'
+                        name='confirmPassword'
+                        value={inputValue.confirmPassword}
+                        onChange={handleChange}
+                        className={`mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                            invalid:border-pink-500 invalid:text-pink-600
+                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500  ${
+                              error.confirmPassword && !inputValue.confirmPassword ? 'border-red-500' : ''
+                            }`}
+                      />
+                      {error.confirmPassword && (
+                        <span className='block text-left text-sm font-medium text-red-700'>
+                          {error.confirmPassword}
                         </span>
                       )}
                     </label>
