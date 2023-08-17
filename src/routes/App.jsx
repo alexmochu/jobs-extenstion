d17362cff0a58c9084135b9be890b3f2e4f52a74
alex.mochu <mochualex4@gmail.com>
2023-08-18 01:27:04 +0300

    if logged dont show homepage

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,19 @@
 import { Fragment } from 'react'
 import HeroSection from '../landingpage/HeroSection.jsx'
-import Features from '../landingpage/Features.jsx'
 // import Stats from '../landingpage/Stats.jsx'
 // import Testimonials from '../landingpage/Testimonials.jsx'
 // import CallToAction from '../landingpage/CallToAction.jsx'
 // import Blog from '../landingpage/Blog.jsx'
 import AppFooter from '../landingpage/AppFooter'
+import { userState } from '../main.jsx'
+import { Navigate } from 'react-router-dom'
 
 const App = () => {
+  const { user } = userState()
+  const isAuthenticated = user.isAuthenticated
+  if (isAuthenticated) {
+    return <Navigate to='/dashboard' />
+  }
   return (
     <Fragment>
       <HeroSection />
